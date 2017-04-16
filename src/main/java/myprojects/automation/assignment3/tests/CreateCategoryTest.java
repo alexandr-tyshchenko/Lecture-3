4182c66c949ce6597bcff97b4d01b87e3a6ee816
Alex.Tyshchenko <alex.tyshchenko89@gmail.com>
2017-04-16 17:05:57 +0300

    Changes from 16/04/17

--- src/main/java/myprojects/automation/assignment3/tests/CreateCategoryTest.java
+++ src/main/java/myprojects/automation/assignment3/tests/CreateCategoryTest.java
@@ -1,81 +1,60 @@
 package myprojects.automation.assignment3.tests;
 
 import myprojects.automation.assignment3.utils.BaseScript;
+import myprojects.automation.assignment3.utils.EventHandler;
+import myprojects.automation.assignment3.utils.GeneralActions;
+import myprojects.automation.assignment3.utils.Properties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Alexandr on 09.04.2017.
  */
-public class CreateCategoryTest extends BaseScript {
+public class CreateCategoryTest extends BaseScript
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        // TODO prepare driver object
+
+        EventFiringWebDriver driver = new EventFiringWebDriver(getDriver());
+        String testCategory= "Test Category";
+        driver.register(new EventHandler());
+
+        GeneralActions actions = new GeneralActions(driver);
+
+        driver.get(Properties.getBaseAdminUrl());
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 
+        actions.login("webinar.test@gmail.com", "Xcg7299bnSmMuRLp9ITw");
 
+        actions.createCategory(testCategory);
 
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = getConfiguredDriver();
+        WebElement filterName = driver.findElement(By.xpath("//input[@name='categoryFilter_name']"));
+        filterName.sendKeys(testCategory);
+        filterName.sendKeys(Keys.ENTER);
+        actions.waitForContentLoad();
 
-        String a = "Test category";
+        List<WebElement> categories = driver.findElements(By.xpath("//tr//td[@class='pointer'][1]"));
+        for (WebElement element : categories)
+        {
+            if (element.getText().equals(testCategory))
+            {
+                System.out.println("Category was created successfully");
+            }
+        }
 
-        driver.navigate().to("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0");
-
-        WebElement emailInput = driver.findElement(By.xpath("//input[@id='email']"));
-        WebElement passwordInput = driver.findElement(By.xpath("//input[@id='passwd']"));
-        WebElement submitButton = driver.findElement(By.xpath("//button[@name='submitLogin']"));
-
-        emailInput.sendKeys("webinar.test@gmail.com");
-        passwordInput.sendKeys("Xcg7299bnSmMuRLp9ITw");
-        submitButton.submit();
-
-        new WebDriverWait(driver, 10)
-        .until(ExpectedConditions.elementToBeClickable(By.xpath("//li[@id='subtab-AdminCatalog']")));
-
-        Actions builder = new Actions(driver);
-        builder.moveToElement(driver.findElement(By.xpath("//li[@id='subtab-AdminCatalog']")));
-        builder.moveToElement(driver.findElement(By.xpath("//li[@id='subtab-AdminCategories']")));
-        builder.click(driver.findElement(By.xpath("//li[@id='subtab-AdminCategories']")));
-        builder.perform();
-
-        new WebDriverWait(driver, 10)
-        .until(ExpectedConditions.elementToBeClickable(By.xpath("//i[@class='process-icon-new']")));
-
-        WebElement createCategory = driver.findElement(By.xpath("//i[@class='process-icon-new']"));
-        createCategory.click();
-
-        new WebDriverWait(driver, 10)
-        .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='name_1']")));
-
-        WebElement categoryName = driver.findElement(By.xpath("//input[@id='name_1']"));
-        categoryName.sendKeys(a);
-
-        WebElement saveCategory = driver.findElement(By.xpath("//i[@class='process-icon-save']"));
-        saveCategory.submit();
-
-        new WebDriverWait(driver, 10)
-        .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='alert alert-success']")));
-
-        WebElement category = driver.findElement(By.xpath("//li[@class='breadcrumb-current']"));
-        category.click();
-
-        new WebDriverWait(driver, 10)
-        .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='categoryFilter_name']")));
-
-        WebElement nameFilter = driver.findElement(By.xpath("//input[@name='categoryFilter_name']"));
-        nameFilter.sendKeys(a);
-        nameFilter.sendKeys(Keys.RETURN);
-
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[contains(text(),'Test category')]")));
-
-        driver.quit();
+        driver.close();
     }
 }
 
--- src/main/java/myprojects/automation/assignment3/utils/GeneralActions.java
+++ src/main/java/myprojects/automation/assignment3/utils/GeneralActions.java
@@ -1,47 +1,83 @@
 package myprojects.automation.assignment3.utils;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by Alexandr on 09.04.2017.
  */
-public class GeneralActions {
+public class GeneralActions
+{
     private WebDriver driver;
     private WebDriverWait wait;
 
-    public GeneralActions(WebDriver driver) {
+
+    public GeneralActions(WebDriver driver)
+    {
         this.driver = driver;
         wait = new WebDriverWait(driver, 30);
     }
 
     /**
      * Logs in to Admin Panel.
+     *
      * @param login
      * @param password
      */
-    public void login(String login, String password) {
+    public void login(String login, String password)
+    {
         // TODO implement logging in to Admin Panel
-        throw new UnsupportedOperationException();
+        driver.get(Properties.getBaseAdminUrl());
+        WebElement email = driver.findElement(By.id("email"));
+        WebElement passwd = driver.findElement(By.id("passwd"));
+        WebElement submit = driver.findElement(By.name("submitLogin"));
+
+        email.sendKeys(login);
+        passwd.sendKeys(password);
+        submit.click();
     }
 
     /**
      * Adds new category in Admin Panel.
+     *
      * @param categoryName
      */
-    public void createCategory(String categoryName) {
-        // TODO implement logic for new category creation
-        throw new UnsupportedOperationException();
+    public void createCategory(String categoryName)
+    {
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        Actions builder = new Actions(driver);
 
+        WebElement catalogueTab = driver.findElement(By.xpath("//li[@data-submenu='9']"));
+        builder.moveToElement(catalogueTab);
+
+        waitForContentLoad();
+        WebElement category = driver.findElement(By.xpath("//li[@data-submenu='11']"));
+        builder.click(category).perform();
+
+        driver.findElement(By.id("desc-category-new")).click();
+        driver.findElement(By.id("name_1")).sendKeys(categoryName);
+
+        jse.executeScript("window.scrollBy(0,450)", "");
+        driver.findElement(By.id("category_form_submit_btn")).click();
+
+        waitForContentLoad();
+        System.out.println("Success  alert is displayed: " + driver.findElement(By.cssSelector(".alert.alert-success")).isDisplayed());
     }
 
     /**
      * Waits until page loader disappears from the page
      */
-    public void waitForContentLoad() {
+    public void waitForContentLoad()
+    {
         // TODO implement generic method to wait until page content is loaded
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("ajax_running")));
 
-        // wait.until(...);
-        // ...
     }
+
+
 }
